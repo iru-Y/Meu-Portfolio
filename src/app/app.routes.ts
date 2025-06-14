6a3d64582be111e343b8f122a7d3ca27fc8fed7b
iruy <yuri_matteus@hotmail.com>
2025-06-13 21:38:30 -0300

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : "",
+        component: HomeComponent
+    }
+];
