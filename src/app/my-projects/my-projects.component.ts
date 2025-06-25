5f23e93a99be8b3d0d51a459f3d7175e8e4c8b97
iruy <yuri_matteus@hotmail.com>
2025-06-25 14:32:47 -0300

    implementação de asterisco decorativo

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from "../components/title/title.component";
 import { LiveDemoCardComponent } from "../components/live-demo-card/live-demo-card.component";
+import { LinkComponent } from "../components/link/link.component";
 
 @Component({
   selector: 'app-my-projects',
-  imports: [TitleComponent, LiveDemoCardComponent],
+  imports: [TitleComponent, LiveDemoCardComponent, LinkComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.css'
 })
