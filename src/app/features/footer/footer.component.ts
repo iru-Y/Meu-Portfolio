a9e895e35064e118d28138354843cc9d40fdb631
iruy <yuri_matteus@hotmail.com>
2025-07-01 14:35:57 -0300

    create: layout responsivo para mobile

--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -4,10 +4,11 @@ import { LinkComponent } from '../../shared/components/link/link.component';
 import { TitleComponent } from '../../shared/components/title/title.component';
 import { FormContactComponent } from './components/form-contact/form-contact.component';
 import { LinkBtnComponent } from "./components/link-btn/link-btn.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [TitleComponent, ContactCardComponent, LinkComponent, FormContactComponent, LinkBtnComponent],
+  imports: [TitleComponent, ContactCardComponent, LinkComponent, FormContactComponent, LinkBtnComponent, NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { LinkComponent } from '../../shared/components/link/link.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [LinkComponent, NgOptimizedImage],
+  imports: [LinkComponent, NgOptimizedImage, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
