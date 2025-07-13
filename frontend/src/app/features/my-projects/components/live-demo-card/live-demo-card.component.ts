014741f32b6bb96be089498e116e3a9ea8d93550
iruy <yuri_matteus@hotmail.com>
2025-07-13 13:01:10 -0300

    release: adicionando efeito de blur nas imagens quando o mouse estiver em cima e mostrar botão de saber mais sobre o projeto

--- frontend/src/app/features/my-projects/components/live-demo-card/live-demo-card.component.ts
+++ frontend/src/app/features/my-projects/components/live-demo-card/live-demo-card.component.ts
@@ -1,9 +1,11 @@
 import { NgClass, NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { LinkBtnComponent } from "../../../footer/components/link-btn/link-btn.component";
+import { LinkComponent } from "../../../../shared/components/link/link.component";
 
 @Component({
   selector: 'app-live-demo-card',
-  imports: [NgOptimizedImage, NgOptimizedImage, NgClass],
+  imports: [NgOptimizedImage, NgOptimizedImage, NgClass, LinkComponent],
   templateUrl: './live-demo-card.component.html',
   styleUrl: './live-demo-card.component.css',
 })
@@ -12,4 +14,9 @@ export class LiveDemoCardComponent {
   @Input() cardWidth: string = '';
   @Input() cardHeight: string = '';
   @Input() imgPath: string = '';
+  isHovered = false;
+
+  onHover(hovered: boolean) {
+    this.isHovered = hovered;
+  }
 }
